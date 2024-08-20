158fe1561f97757c489c12c9aa9a6d6ba328632b
Cleidson009 <gabrielcleidson21@gmail.com>
2024-08-20 10:55:40 -0400

    Pyton2
    AQUI FOI FEITO UM CODIGO DE PHYTON NO DIA 20 DE AGOSTO, NO SENAC.

---
+++ index.py
@@ -0,0 +1,25 @@
+a = 4273
+b = 300342
+
+
+soma = a + b 
+
+subitracao = a - b  
+multiplicacao = a * b 
+divisao = a / b 
+igual = a == b
+diferente = a != b
+menor_que = a < b
+maior_que = a > b
+
+sequencia = [1, 2, 9, 4, 5, 6, 7]
+nao_esta_na_sequencia = 3 not in sequencia
+
+lista1 = [1, 2, 8]
+lista2 = [1, 3, 6]
+resultado_is_not = lista1 is not lista2
+
+print(resultado_is_not)
+
+
+
